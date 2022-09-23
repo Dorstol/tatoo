c57864ebc397bae2965ef41ce2a64133ae961592
Oleh <olehsofronov@gmail.com>
2022-09-23 22:32:00 +0300

    Splited settings

---
+++ src/config/settings/dev.py
@@ -0,0 +1,8 @@
+from config.settings.base import *  # NOQA:
+
+CURRENT_ENV = "DEV"
+print(CURRENT_ENV)
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
---
+++ src/config/settings/prod.py
@@ -0,0 +1,7 @@
+from config.settings.base import *  # NOQA:
+
+
+CURRENT_ENV = "PROD"
+print(CURRENT_ENV)
+
+DEBUG = False
